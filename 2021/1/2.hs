0e66d035939572fddd12e6575df47cec5d9a60ff
DrVilepis <63553760+DrVilepis@users.noreply.github.com>
2021-12-02 12:59:34 +0200

    Whoops brain go brrr

--- 2021/1/2.hs
+++ 2021/1/2.hs
@@ -2,7 +2,7 @@ import Data.List
 
 groups :: [Int] -> [[Int]]
 groups xs
-    | (length xs) > 4 = (take 3 xs):(groups (drop 4 xs))
+    | (length xs) > 3 = (take 3 xs):(groups (drop 1 xs))
     | otherwise = [take 3 xs]
 
 
@@ -10,8 +10,7 @@ solve :: [Int] -> Int
 solve xs = length 
     $ filter (\(x,y) -> (x < y)) 
     $ (\l -> zip l (tail l)) 
-    $ concat
-    $ transpose 
-    $ map (\x -> map (sum) $ groups $ reverse $ drop (abs x-3) $ reverse $ drop x xs) [0..3]
+    $ map (sum)
+    $ groups xs
 
 main = interact $ show . solve . map (read) . lines
