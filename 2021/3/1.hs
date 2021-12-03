a615ecd28e3950191a83c41748ab20619a54d212
DrVilepis <63553760+DrVilepis@users.noreply.github.com>
2021-12-04 01:40:01 +0200

    More cleanup

--- 2021/3/1.hs
+++ 2021/3/1.hs
@@ -1,14 +1,15 @@
 import Data.List
 import Data.Ord
 
+bintodec :: Int -> Int
 bintodec 0 = 0
 bintodec i = 2 * bintodec (div i 10) + (mod i 10)
 
 solve :: [String] -> Int
-solve xs = product
-    $ map (bintodec . read)
-    $ transpose 
-    $ map (map (head) . sortBy (comparing length) . group . sort)
-    $ transpose xs 
+solve = product
+    . map (bintodec . read)
+    . transpose 
+    . map (map (head) . sortBy (comparing length) . group . sort)
+    . transpose
 
 main = interact $ show . solve . lines
--- 2021/3/2.hs
+++ 2021/3/2.hs
@@ -1,6 +1,7 @@
 import Data.List
 import Data.Ord
 
+bintodec :: Int -> Int
 bintodec 0 = 0
 bintodec i = 2 * bintodec (div i 10) + (mod i 10)
 
