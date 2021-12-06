9489d087810b88245762ae6dc8b641118cbc9ce2
DrVilepis <63553760+DrVilepis@users.noreply.github.com>
2021-12-06 14:50:43 +0200

    Added day 6

---
+++ 2021/6/2.rs
@@ -0,0 +1,18 @@
+fn main() {
+    let mut input = String::new();
+    std::io::stdin().read_line(&mut input);
+    let mut input = input.trim().split(',').map(|num| num.parse::<usize>().unwrap()).collect::<Vec<_>>();
+    let mut fish = [0u64;9];
+    for i in input {
+        fish[i] += 1;
+    }
+    for _ in 0..256 {
+        let tmp = fish[0];
+        for i in 0..8usize {
+            fish[i] = fish[i+1];
+        }
+        fish[8] = tmp;
+        fish[6] += tmp;
+    }
+    println!("{}",fish.iter().sum::<u64>());
+}
