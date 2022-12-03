05279b8eabda85b41de5a702046574dfb58f709c
DrVilepis <jarvinenville606@gmail.com>
2022-12-03 16:36:02 +0200

    Forgor

--- 2022/03/1.hs
+++ 2022/03/1.hs
@@ -2,4 +2,4 @@ import Control.Applicative
 import Data.List
 import Data.Maybe
 
-main = interact $ show . sum . map (fromMaybe 0 . flip lookup (zip (['a'..'z']++['A'..'Z']) [1..])) . map (head . uncurry intersect . liftA2 splitAt ((`div` 2) . length) id) . lines
+main = interact $ show . sum . map (fromMaybe 0 . flip elemIndex (' ':(['a'..'z']++['A'..'Z']))) . map (head . uncurry intersect . liftA2 splitAt ((`div` 2) . length) id) . lines
